1a360c03dac121c9a3c0bc52a5ab257fde49e17f
den021912 <usakovdenis440@gmail.com>
2024-08-28 19:11:53 +0400

    Add files via upload

---
+++ Django_project/shop/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from shop.forms import UserRegister
+from .models import Game, Buyer
+
+
+# Create your views here.
+
+def class_shop(request):
+    title_class_shop = 'Магазин'
+    context = {
+        'title_class_template': title_class_shop
+    }
+    return render(request, 'class_shop.html', context=context)
+
+
+def games(request):
+    title_games = 'Игры'
+    buy = 'Купить'
+    gamess = Game.objects.all()
+    context = {
+        'title_games': title_games,
+        'buy': buy,
+        'gamess': gamess
+    }
+    return render(request, "games.html", context=context)
+
+
+def basket(request):
+    title_basket = 'Корзина'
+    context = {
+        'title_basket': title_basket,
+    }
+    return render(request, "basket.html", context=context)
+
+
+def menu(request):
+    mane_page = 'Главная страница'
+    magazine_page = 'Игры на выбор'
+    basket_page = 'Корзина'
+    context = {
+        'mane_page': mane_page,
+        'magazine_page': magazine_page,
+        'basket_page': basket_page
+    }
+    return render(request, "menu.html", context=context)
+
+
+def sign_up_by_html(request):
+    buyers = Buyer.objects.all()
+    users = set()
+    for buyer in buyers:
+        users.add(buyer.name)
+    info = {}
+    len_info = len(info)
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        username1 = request.POST.get('username')
+        password1 = request.POST.get('password')
+        repeat_password1 = request.POST.get('repeat_password')
+        age1 = request.POST.get('age')
+        if form.is_valid():
+            if username1 in users:
+                info['error'] = 'Пользователь уже существует'
+                return render(request, 'registration_page.html', context=info)
+            elif repeat_password1 != password1:
+                info['error'] = 'Пароли не совпадают'
+                return render(request, 'registration_page.html', context=info)
+            elif int(age1) < 18:
+                info['error'] = 'Вы должны быть старше 18'
+                return render(request, 'registration_page.html', context=info)
+            else:
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                repeat_password = form.cleaned_data['repeat_password']
+                age = form.cleaned_data['age']
+                Buyer.objects.create(name=username, age=age)
+                info = {}
+                return render(request, 'registration_page.html',
+                              context={'wellcome': f'Приветствуем, {username1}!'})
+    else:
+        form = UserRegister()
+    return render(request, 'registration_page.html')
+
+
